bb7f8d2d6275c741fe49719fe67ae40950e06fdd
KyawZinThant22 <kyawthantver2@gmail.com>
2022-10-12 22:33:06 +0630

    35 of 206 ts config

--- app.js
+++ app.js
@@ -1,13 +1,2 @@
-var userInput;
-var userName;
-userInput = 5;
-userInput = "Brian";
-if (typeof userInput === "string") {
-    userName = userInput;
-}
-//never
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-    while (true) { }
-}
-generateError("An error occured", 404);
+"use strict";
+console.log("hello ts config");
