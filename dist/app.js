e971dd4519d929b08eb3657bfd51f5ea2486f08d
KyawZinThant22 <kyawthantver2@gmail.com>
2022-10-15 12:04:13 +0630

    50 of 206 arrow function

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,16 @@
 "use strict";
-let name1 = "kyawzinthant";
-name1 =
-    "what is the meaning of life and how can we survive in this cruel world";
-const name2 = "kyawkyaw";
-// name2="mgmg"
-let namename = "kyaw";
-function gen() {
-    console.log("function", namename);
-}
-console.log(namename);
-gen();
+// let name1 = "kyawzinthant";
+// name1 =
+//   "what is the meaning of life and how can we survive in this cruel world";
+// const name2 = "kyawkyaw";
+// // name2="mgmg"
+// var namename = "kyaw";
+// function gen() {
+//   console.log("function", namename);
+// }
+// console.log(namename);
+// gen();
+const add = (a, b) => a + b;
+const printOutPut = (outPut) => console.log(outPut);
+printOutPut(add(5, 7));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,23 @@
-let name1 = "kyawzinthant";
+// let name1 = "kyawzinthant";
 
-name1 =
-  "what is the meaning of life and how can we survive in this cruel world";
+// name1 =
+//   "what is the meaning of life and how can we survive in this cruel world";
 
-const name2 = "kyawkyaw";
-// name2="mgmg"
+// const name2 = "kyawkyaw";
+// // name2="mgmg"
 
-var namename = "kyaw";
-function gen() {
-  console.log("function", namename);
-}
+// var namename = "kyaw";
+// function gen() {
+//   console.log("function", namename);
+// }
 
-console.log(namename);
+// console.log(namename);
 
-gen();
+// gen();
+
+const add = (a: number, b: number): number => a + b;
+
+const printOutPut: (a: number | string) => void = (outPut) =>
+  console.log(outPut);
+
+printOutPut(add(5, 7));
