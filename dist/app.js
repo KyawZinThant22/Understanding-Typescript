44491ae4fd272b1ffd0d1c2496cad536f5e47433
KyawZinThant22 <kyawthantver2@gmail.com>
2022-10-19 20:54:28 +0630

    84 of 206 Type Gurds

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,17 @@
 "use strict";
 // intersection types mean combiming types
 const brian = {
-    name: "kyaw",
+    name: "kyaw zin thant",
     privileage: ["server", "accept"],
     startDate: new Date(),
 };
+function printEmployeInformation(emp) {
+    console.log("Name ", emp.name);
+    if ("privileage" in emp) {
+        console.log("privileage ", emp.privileage);
+    }
+    if ("startDate" in emp) {
+        console.log("Start Date ", emp.startDate);
+    }
+}
+printEmployeInformation(brian);
--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,24 @@ interface CombinedTypes extends Admin, Employe {}
 type CombinationofTypes = Admin & Employe;
 
 const brian: CombinedTypes = {
-  name: "kyaw",
+  name: "kyaw zin thant",
   privileage: ["server", "accept"],
   startDate: new Date(),
 };
+
+type Combinale = string | number;
+type Numeric = boolean | number;
+
+type UnknownType = Admin | Employe;
+
+function printEmployeInformation(emp: UnknownType) {
+  console.log("Name ", emp.name);
+  if ("privileage" in emp) {
+    console.log("privileage ", emp.privileage);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date ", emp.startDate);
+  }
+}
+
+printEmployeInformation(brian);
